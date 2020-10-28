da6222c663102866ef0bd7119a2e72d2d0588bc8
DEV-PROG-specs <devendra.srivastava_cs19@gla.ac.in>
2020-10-27 20:27:09 -0700

    add variable month which gives name of month

--- src/definition/Main.java
+++ src/definition/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         int currentMonth = new GregorianCalendar().get(Calendar.MONTH);
         int todayDate = new GregorianCalendar().get(Calendar.DATE);
+        String month = new GregorianCalendar().getDisplayName(Calendar.MONTH,Calendar.LONG,Locale.ENGLISH).trim().toUpperCase();
 
     }
 }
